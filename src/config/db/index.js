a25e5fdca302d8ab51b08afb94e25f4502ab2324
Your NameThanhNhon <thanhnhonn7@gmial.com>
2021-10-16 15:15:37 +0700

    fd

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost/chatApp');
+        await mongoose.connect('mongodb+srv://thanhnhon:nhon171@cluster0.rmp3v.mongodb.net/chatApp?retryWrites=true&w=majority');
         console.log('Connected successfully!')
     } catch (error) {
         console.log('Connect failure!');
